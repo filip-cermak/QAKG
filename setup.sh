8b555cdabe0c3f53f0748b3b025b5c5872888cfb
F. Cermak <fc469@air208.eng.cam.ac.uk>
2022-08-26 09:18:17 +0100

    specifying python  version

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ source venv/bin/activate
 
 bash input.sh
 
-pip install spacy==2.1.0 # 2.3.7, 2.1.9 not working, 2.1.0 working
-spacy download en_core_web_lg
-pip install neuralcoref
+python3.7 -m pip install spacy==2.1.0 # 2.3.7, 2.1.9 not working, 2.1.0 working
+python3.7 -m spacy download en_core_web_lg
+python3.7 -m pip install neuralcoref
 
-pip install stanza
+python3.7 -m pip install stanza
 
-python main.py
+python3.7 -m  main.py

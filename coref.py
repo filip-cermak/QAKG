442f960cf3fe220d42db241ac3c9cf3c58aa6a83
filip-cermak <fcermak64@gmail.com>
2022-10-20 10:31:08 +0100

    verbous coref

--- coref.py
+++ coref.py
@@ -4,10 +4,15 @@ def resolve_coreference(nlp_object, input_text):
   return output_text
 
 def resolve_question_list_coreference(nlp, question_list):
+  
+  l = len(question_list)
+  print(f'Total number of questions: {l}')
 
-  for q in question_list:
+  for i,q in enumerate(question_list):
     q.context_cor_resolved = resolve_coreference(nlp, q.context)
     q.question_with_answer_cor_resolved = resolve_coreference(nlp, q.question_with_answer)
     q.question_with_distractors_cor_resolved = [resolve_coreference(nlp, i) for i in q.question_with_distractors]
+
+    print(f'Question {i} out of {l}')
 
   return question_list

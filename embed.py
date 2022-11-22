33ae3c4f2cce36df71780f7c52deed2dc71f9826
filip-cermak <fcermak64@gmail.com>
2022-11-22 22:48:43 +0000

    adding embed functionality

--- embed.py
+++ embed.py
@@ -12,4 +12,60 @@ def embed(s):
     tokens = tokenizer.tokenize(s)
     embeddings = pipe(s)
 
-    return tokens, embeddings
+    chr_spans_with_embeddings = {}
+
+    for i, e in enumerate(embeddings):
+        start = tokenizer(s).token_to_chars(i).start
+        end = tokenizer(s).token_to_chars(i).end
+        chr_spans_with_embeddings[(start, end)] = e
+
+    return chr_spans_with_embeddings
+
+def chr_spn_matcher(ref_chr_spn, list_of_chr_spns):
+    # returns all char_span-s from the list that match
+    # the reference char_span
+    # char span is just a length 2 tuple
+
+    matching_chr_spns = [x for x in list_of_chr_spns if contains_chr_spn(ref_chr_spn, x)]
+    return matching_chr_spns
+
+def contains_chr_spn(ref_chr_spn, test_chr_spn):
+
+    if (test_chr_spn[0] >= ref_chr_spn[0]) and (test_chr_spn[1] <= ref_chr_spn[1]):
+        return True
+
+    if (test_chr_spn[0]+1 >= ref_chr_spn[0]) and (test_chr_spn[1] <= ref_chr_spn[1]):
+        return True
+
+    return False
+
+def add_embeddings_to_question_helper(q):
+
+    #context
+    chr_spans_with_embeddings = embed(q.context_cor_resolved)
+    enrich_triples(q.context_triples, chr_spans_with_embeddings)
+
+    #q+a
+    chr_spans_with_embeddings = embed(q.context_cor_resolved)
+    enrich_triples(q.question_with_answer_triples, chr_spans_with_embeddings)
+
+    #q+d - 0/1/2
+    for i in range(3):
+        chr_spans_with_embeddings = embed(q.question_with_distractors_cor_resolved[i])
+        enrich_triples(q.question_with_distractors_triples[i], chr_spans_with_embeddings)
+
+def add_embeddings_to_questions(q_list):
+    for q in q_list:
+        add_embeddings_to_question_helper(q)
+
+def enrich_triples(triples, chr_spans_with_embeddings):
+    for t in triples:
+        for chr_spn in t.subject_words:
+            t.subject_embeds.append([chr_spans_with_embeddings[k] for k in chr_spn_matcher(chr_spn, list(chr_spans_with_embeddings.keys()))])
+
+        for chr_spn in t.relation_words:
+            t.relation_embeds.append([chr_spans_with_embeddings[k] for k in chr_spn_matcher(chr_spn, list(chr_spans_with_embeddings.keys()))])
+
+        for chr_spn in t.object_words:
+            t.object_embeds.append([chr_spans_with_embeddings[k] for k in chr_spn_matcher(chr_spn, list(chr_spans_with_embeddings.keys()))])
+
--- test_embed.py
+++ test_embed.py
@@ -1,12 +1,14 @@
-"""
 import embed
 
-t, e = embed.embed("Let's try to tokenize!")
+def test_contains_chr_spn():
 
-print(t)
+    assert embed.contains_chr_spn((1, 3), (1, 3)) == True
+    assert embed.contains_chr_spn((1, 3), (1, 2)) == True
+    assert embed.contains_chr_spn((1, 3), (0, 3)) == True 
+    assert embed.contains_chr_spn((1, 3), (2, 3)) == True 
+    assert embed.contains_chr_spn((5, 6), (6, 7)) == False
+    assert embed.contains_chr_spn((5 , 8), (1 ,5)) == False
 
-def test_embed():
-
-    print(embed.embed("This is a test.")) 
-
-"""
+def test_chr_spn_matcher():
+    assert embed.chr_spn_matcher((1,3), [(1,3), (1,2), (0,3), (3,4)]) == [(1,3), (1,2), (0,3)]
+ 
